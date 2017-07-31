1799ac4ae2e1a9034b9356111e2b87634b19140c
chloelxx <chloe.liu@blemobi.com>
2017-08-01 00:04:40 +0800

    修改打包文件

--- webpack-test/webpack.config.js
+++ webpack-test/webpack.config.js
@@ -1,17 +1,20 @@
 var htmlWebpackPlugin=require("html-webpack-plugin");
 var webpack = require("webpack");
+var webpackDevMiddleware = require("webpack-dev-middleware");
+var publicPath = 'http://localhost:8080/';
+var hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true';
 //var path = require("path");
 module.exports={
 	entry:{
-		main: "./src/script/main.js",
-		a: "./src/script/a.js",
-		index: "./src/script/index.js",
+		main: ["./src/script/main.js",hotMiddlewareScript],
+		a:[ "./src/script/a.js",hotMiddlewareScript],
+		index:[ "./src/script/index.js",hotMiddlewareScript]
 	},
 	output:{
 		path: "./dist",
 		//filename: "js/[name]-compress.js",
 		publicPath : '/asset/',
-		filename:'js/[name]-compress.js'
+		filename:'js/[id]-[hash].js'
 		//publicPath:"http://cdn.com/",//表示你的项目上线后的地址。http://cdn.com/将替换"./dist",地址
 	},
 	//加载器  
@@ -32,11 +35,14 @@ module.exports={
 	  ]
   },  
 	plugins:[  //注意这个插件是一个数组
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
 		new htmlWebpackPlugin({
 			//title: '简单页面',
 			filename: "index-compress.html",
 			template:"index.html",
-			inject:false,  
+			inject:false,
 			//inject:'head',  //指定js是放在head中还是body中 inject的值可以为false表示不把js注入到head或者body中
 			title: "this is a webpack",
 			/*date:new Date(),*/
@@ -49,8 +55,8 @@ module.exports={
 			//title: '简单页面',
 			filename: "a-compress.html",
 			template:"a.html",
-			inject:false,  
-			//inject:'head',  //指定js是放在head中还是body中 inject的值可以为false表示不把js注入到head或者body中
+			//inject:false,
+			inject:'head',  //指定js是放在head中还是body中 inject的值可以为false表示不把js注入到head或者body中
 			title: "this is a webpack",
 			date:new Date(),
 			//minify:{
